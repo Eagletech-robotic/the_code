b7186a9208ecacedd9997d50cda74b148624f293
Thibault-Monnier <thibaultmonni@gmail.com>
2025-01-18 16:19:50 +0100

    Fix angleBetweenVectors prototype in projection.h

--- helloworld2/Core/Inc/robotic/projection.h
+++ helloworld2/Core/Inc/robotic/projection.h
@@ -11,7 +11,7 @@
 void projectOnPlane(const float A[3], const float M[3], float M2D[2]);
 
 float angleBetween2D(const float A[2], const float B[2]);
-float angleBetweenVectors(float v1[3], float v2[3]);
+float angleBetweenVectors(const float v1[3], const float v2[3]);
 float computeHeading(float mag[3], float acc[3]);
 void calibrate_nr(float mag[3]);
 void stat_nr(float data);
