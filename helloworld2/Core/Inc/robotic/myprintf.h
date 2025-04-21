0924a552f9223b4c3a7511a9952c92537694bb9b
antmonnier <monniera@gmail.com>
2025-04-21 16:30:52 +0200

    myprintf now resolves to standard printf, unless SLOW_MYPRINTF is enabled

--- helloworld2/Core/Inc/robotic/myprintf.h
+++ helloworld2/Core/Inc/robotic/myprintf.h
@@ -7,6 +7,9 @@
 
 #pragma once
 #include <stdio.h>
+
+#ifdef SLOW_MYPRINTF
+// Print every n messages. For use on MCU.
 #define myprintf(fmt, ...)           \
     do {                                     \
         static int counter_##__LINE__ = 0;   \
@@ -15,3 +18,7 @@
         }                                    \
 		counter_##__LINE__++;                \
     } while(0)
+#else
+// Standard printf. For use on a PC.
+#define myprintf(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#endif
