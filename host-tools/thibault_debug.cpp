e51e682f7861aa7bcb41ef7cb2350130779f810b
antmonnier <monniera@gmail.com>
2025-04-23 12:58:44 +0200

    Forgotten in previous commit

--- host-tools/thibault_debug.cpp
+++ host-tools/thibault_debug.cpp
@@ -3,14 +3,14 @@
 #include "utils/debug.hpp"
 
 // Access the potential field defined in thibault.cpp
-extern float potential_field[P_FIELD_W][P_FIELD_H];
+extern float potential_field[FIELD_WIDTH_SQ][FIELD_HEIGHT_SQ];
 
 int main(int argc, char **argv) {
     config_t config;
 
     thibault_top_init(&config);
 
-    visualize_potential_field(potential_field, P_FIELD_W, P_FIELD_H);
+    visualize_potential_field(potential_field, FIELD_WIDTH_SQ, FIELD_HEIGHT_SQ);
 
     return 0;
 }
