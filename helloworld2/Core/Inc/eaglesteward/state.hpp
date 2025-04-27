5b27529e06c9b9580f75ae7607639b12e0e950aa
antmonnier <monniera@gmail.com>
2025-04-27 20:02:07 +0200

    Use coordinates from camera

--- helloworld2/Core/Inc/eaglesteward/state.hpp
+++ helloworld2/Core/Inc/eaglesteward/state.hpp
@@ -3,10 +3,19 @@
 
 #include "robotic/pid.hpp"
 
+enum class Color { BLUE = 0, YELLOW = 1 };
+
 typedef struct state_t {
+    enum Color color;
+    // IMU coordinate system
     float x_m;
     float y_m;
     float theta_deg;
+    // IMU to field coordinate transformation
+    float theta_offset_deg; // Rotation offset between IMU and field
+    float x_offset_m;       // X translation offset after rotation
+    float y_offset_m;       // Y translation offset after rotation
+    // ...
     int target; // for rectangle test
     float filtered_tof_m;
     // --- Ecrit par retour
@@ -26,3 +35,5 @@ typedef struct state_t {
 
 void print_state(state_t *state);
 void state_init(state_t *);
+
+void convert_from_imu_to_field(state_t &state, float &out_x, float &out_y, float &out_theta);
