8e03203d099c441028c3ef988b28d890e002ddcc
antmonnier <monniera@gmail.com>
2025-05-04 12:03:27 +0200

    Limit output to 15 lines for readability

--- check_stack.sh
+++ check_stack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-find . -name "*.su" -exec cat {} + | perl -pe 's{^(.*\))(\s+)(\d+)(\s+)(static.*)}{$3 $1$2$4$5}' | sort -n 
+find . -name "*.su" -exec cat {} + | perl -pe 's{^(.*\))(\s+)(\d+)(\s+)(static.*)}{$3 $1$2$4$5}' | sort -n | tail -n 15 
