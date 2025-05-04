48f56ab70a5f9e05648a7053e8b7f4d6e1ad23c3
antmonnier <monniera@gmail.com>
2025-05-04 13:48:31 +0200

    Clean up thibault_debug

--- host-tools/thibault_debug.cpp
+++ host-tools/thibault_debug.cpp
@@ -1,17 +1,10 @@
-#include "eaglesteward/guidance/thibault.hpp"
 #include "eaglesteward/state.hpp"
 #include "utils/constants.hpp"
 #include "utils/debug.hpp"
 
-extern state_t thibault_state;
-
 int main(int argc, char **argv) {
-    config_t config{};
-
-    thibault_top_init(config);
-
-    auto const world = thibault_state.world;
-    visualize_potential_field(world.potential(), FIELD_WIDTH_SQ, FIELD_HEIGHT_SQ);
+    State state;
+    visualize_potential_field(state.world.potential(), FIELD_WIDTH_SQ, FIELD_HEIGHT_SQ);
 
     return 0;
 }
