29eb37ec789bf0748023cb9386c99c6136f7df00
Thibault-Monnier <thibaultmonni@gmail.com>
2025-05-10 21:14:32 +0200

    Highly optimze bounded pqueue

--- falcon/Core/Inc/utils/bounded_pqueue.hpp
+++ falcon/Core/Inc/utils/bounded_pqueue.hpp
@@ -1,28 +1,25 @@
 #pragma once
-
-// Template parameters:
-//   T         : value type stored in the queue
-//   Capacity  : maximum number of elements (must be > 0)
-//   Compare   : strict weak ordering functor (defaults to std::less<T>)
-
 #include <array>
 #include <concepts>
 #include <functional>
+#include <limits>
 #include <stdexcept>
 #include <utility>
 
 template <typename T, std::size_t Capacity, typename Compare = std::less<T>>
     requires(Capacity > 0) && std::strict_weak_order<Compare, T, T>
-class BoundedPriorityQueue {
+class BoundedPriorityQueue : Compare {
   public:
     using value_type = T;
     using size_type = std::size_t;
     using reference = value_type &;
     using const_reference = const value_type &;
 
+    static_assert(Capacity <= (std::numeric_limits<size_type>::max() >> 1), "size_type too small for this capacity");
+
     static constexpr size_type capacity = Capacity;
 
-    constexpr BoundedPriorityQueue() noexcept(std::is_nothrow_default_constructible_v<Compare>) = default;
+    constexpr BoundedPriorityQueue() noexcept = default;
 
     [[nodiscard]] constexpr bool empty() const noexcept { return size_ == 0; }
     [[nodiscard]] constexpr size_type size() const noexcept { return size_; }
@@ -31,58 +28,52 @@ class BoundedPriorityQueue {
 
     [[nodiscard]] constexpr const_reference top() const { return data_[0]; }
 
-    constexpr void push(const value_type &value) { do_push(value); }
-
-    constexpr void push(value_type &&value) { do_push(std::move(value)); }
+    constexpr void push(const value_type &v) { emplace(v); }
+    constexpr void push(value_type &&v) { emplace(std::move(v)); }
 
     template <typename... Args> constexpr void emplace(Args &&...args) {
-        data_[size_] = value_type(std::forward<Args>(args)...);
-        sift_up(size_++);
+        // Create the hole at the end, then sift it up.
+        value_type tmp(std::forward<Args>(args)...);
+        sift_up_hole(size_, std::move(tmp));
+        ++size_;
     }
 
     constexpr void pop() {
-        --size_;
-        if (size_ > 0) {
-            data_[0] = std::move(data_[size_]);
-            sift_down(0);
-        }
+        // Move last element to root hole, then sift it down.
+        value_type tmp(std::move(data_[--size_]));
+        sift_down_hole(0, std::move(tmp));
     }
 
   private:
-    std::array<value_type, capacity> data_{}; // zero‑initialised for constexpr
+    std::array<value_type, capacity> data_{};
     size_type size_{0};
-    Compare comp_{}; // comparator instance
 
-    constexpr void do_push(value_type &&value) {
-        data_[size_] = std::forward<value_type>(value);
-        sift_up(size_++);
-    }
+    // Convenience: expose comparator as a function-call operator
+    [[nodiscard]] constexpr const Compare &cmp() const noexcept { return *this; }
 
-    constexpr void sift_up(size_type idx) noexcept {
-        while (idx > 0) {
-            size_type parent = (idx - 1) / 2;
-            if (!comp_(data_[parent], data_[idx]))
-                break; // parent has higher/equal priority
-            std::swap(data_[parent], data_[idx]);
-            idx = parent;
+    constexpr void sift_up_hole(size_type hole, value_type &&val) noexcept {
+        const auto &less = static_cast<const Compare &>(*this);
+        while (hole) {
+            size_type parent = (hole - 1) >> 1;
+            if (!less(data_[parent], val)) [[likely]]
+                break;
+            data_[hole] = std::move(data_[parent]); // pull parent down
+            hole = parent;
         }
+        data_[hole] = std::move(val); // drop into place
     }
 
-    constexpr void sift_down(size_type idx) noexcept {
-        while (true) {
-            size_type left = idx * 2 + 1;
+    constexpr void sift_down_hole(size_type hole, value_type &&val) noexcept {
+        size_type half = size_ >> 1; // last internal node
+        while (hole < half) {
+            size_type left = (hole << 1) + 1;
             size_type right = left + 1;
-            if (left >= size_)
-                break; // no children
-
-            size_type best = left;
-            if (right < size_ && comp_(data_[left], data_[right])) {
-                best = right; // right child has higher priority
-            }
-            if (!comp_(data_[idx], data_[best]))
-                break; // heap invariant satisfied
-            std::swap(data_[idx], data_[best]);
-            idx = best;
+            size_type best = (right < size_ && cmp()(data_[left], data_[right])) ? right : left;
+            if (!cmp()(val, data_[best])) [[likely]]
+                break;
+            data_[hole] = std::move(data_[best]); // push child up
+            hole = best;
         }
+        data_[hole] = std::move(val);
     }
 };
